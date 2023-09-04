5640392d32aa6cf4a9f70724e0d0e7ebae053e31
mauro zanetti <mauro.zanetti@accenture.com>
2023-09-04 13:41:55 +0000

    key

--- srv/service.js
+++ srv/service.js
@@ -4,7 +4,14 @@ module.exports = cds.service.impl(async function() {
 
 	const service = await cds.connect.to('API_BUSINESS_PARTNER');
 
-	this.on('READ', 'A_AddressEmailAddress', req => {
-		return service.run(req.query);
+	this.on('READ', 'A_AddressEmailAddress', async req => {
+		const aux = await service.run(req.query);
+		// const aux = await service.send({
+        //     query: req.query,
+        //     headers: {
+        //         'APIKey': cds.env.requires.API_BUSINESS_PARTNER.credentials.headers.APIKey
+        //     }
+        // });
+		return aux;
 	});
 });
