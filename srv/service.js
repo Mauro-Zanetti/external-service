2154d3b833f2f124acb97d32f0c061e942a61744
mauro zanetti <mauro.zanetti@accenture.com>
2023-09-06 13:37:08 +0000

    priv

--- srv/service.js
+++ srv/service.js
@@ -1,17 +1,16 @@
 const cds = require('@sap/cds');
+const constants = require('./Constants.json');
 
 module.exports = cds.service.impl(async function() {
 
 	const service = await cds.connect.to('API_BUSINESS_PARTNER');
 
 	this.on('READ', 'A_AddressEmailAddress', async req => {
-		const aux = await service.run(req.query);
-		// const aux = await service.send({
-        //     query: req.query,
-        //     headers: {
-        //         'APIKey': cds.env.requires.API_BUSINESS_PARTNER.credentials.headers.APIKey
-        //     }
-        // });
-		return aux;
+		return await service.send({
+            query: req.query,
+            headers: {
+                'APIKey': constants.APIKey
+            }
+        });
 	});
 });
